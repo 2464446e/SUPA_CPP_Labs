a4e317aaddfcd90f1929b9c00fa7e055de9a2adc
Ewan Eaglesham <153522957+2464446e@users.noreply.github.com>
2024-12-12 14:55:29 +0000

    12.12 2 almost finished

--- Exercises2024/Ex3_4/Test.cxx
+++ Exercises2024/Ex3_4/Test.cxx
@@ -1,11 +1,14 @@
 #include <filesystem>
 #include "FiniteFunctions.h"
+#include <map>
+#include <iomanip>
 #include <fstream>
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <math.h>
 #include<cstdlib>
+#include <random>
 #include <sstream>
 #pragma once
 #include "gnuplot-iostream.h"
@@ -64,6 +67,42 @@ double callFunction(double x) {
 
 
 //double FiniteFunction::callFunction(double x){FiniteFunction::callFunction(x);{return this->func2(x, 5, 6);}};
+
+//Now, wish to impliment this such that we generate an arbitrarily long set of data (vector)
+// which can be our x:
+vector<double> generator(int size){
+    cout << "running function"<<endl;
+   vector<double> vectobject;
+   int shape = size;
+   std::random_device genx{};
+   std::mt19937 distrx{genx()};
+   std::uniform_real_distribution<float> arb{-10, 10};
+   std::uniform_real_distribution<float> STD{0, 10};
+   float val = arb(distrx);
+   float std = STD(distrx);
+   float initialx = func2(val, 1.75, 0.05);
+   for(int i =0; i <= shape; ++i){
+std:random_device geny{};
+std::mt19937 distry{geny()};
+std::normal_distribution<float> d{val,std};
+float y = func2(d(geny) ,1.75,0.05);
+float x;
+if(i <1){x = initialx;}else{x= vectobject[i-1];};
+double div = y/x;
+double comp = 1;
+float Amin = std::min(div, comp);
+ // Now, generating T:
+std::random_device Tgen{};
+std::mt19937 creating{Tgen()};
+std::uniform_real_distribution<float> arb{0, 1};
+float T = arb(creating);
+if(T<Amin){
+    vectobject.push_back(y); }
+    //cout <<y <<endl;}
+    else{vectobject.push_back(x);};}
+    //cout<< x<< endl;}
+return vectobject;}
+
 int main(){
 
 // need to read in mystery data:
@@ -92,9 +131,50 @@ myfunction.printInfo();
 //plots these bins (y's are the density of the bin)
 myfunction.plotData(mystvec, 250, true); 
 //now need to redefine the integration function to output correctly normalised plot. 
-//std::cout << func3(1, 0,3,4,5) <<std::endl;
-return 0;
+//std::cout << func3(1, 0,3,4,5) <<std::endl;;
 
+// Now, attempt to create random numbers:
+unsigned int seed = 86;
+unsigned int nut = 5;
+std::mt19937 mtEngine{seed};
+std::uniform_real_distribution<float> uniform{-10, 10};
+std::uniform_real_distribution<float> STD{0, 10};
+float randx = uniform(mtEngine);
+float std = STD(mtEngine);
+cout << "this: " <<randx << " and this: "<< std << "." << endl;
+std:random_device rd{};
+std::mt19937 gen{rd()};
+std::normal_distribution<float> d{randx,std };
+float randy = d(gen);
+std::cout << randy << std::endl;
+float y = func2(randy ,1.75,0.05);
+float x = func2( randx,1.75,0.05);
+double div =  y/x;
+double comp = 1;
+cout << y/x <<endl;
+float Amin = std::min(div, comp);
+cout << Amin << endl;
+ // Now, generating T:
+std::random_device Tgen{};
+std::mt19937 creating{Tgen()};
+std::uniform_real_distribution<float> arb{0, 1};
+float T = arb(creating);
+cout << "T = "<< T <<endl;
+float xi;
+if(T<Amin){
+    xi = y; }
+    else{xi = x;};
+cout <<x<<", " << ", "<< y<< ", "<< xi <<endl;
+
+
+//Have created a function called generator which creates a double of length (), using the function we specified
+vector<double> metropolis = generator(1000);
+//Final task is to try and impliment this sampled data (y values for the function and their corresponding x's
+//i.e. random numbers suupplied, to get a sample trendline. )
+return 0; 
 }
+
+
+
 
 
